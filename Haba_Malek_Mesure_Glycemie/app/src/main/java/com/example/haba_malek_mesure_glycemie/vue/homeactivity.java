f9642548b06fb17282ac0f922c7948723c019051
Malek-Haba <147167905+Malek-Haba@users.noreply.github.com>
2023-12-03 20:48:14 +0100

    k

---
+++ Haba_Malek_Mesure_Glycemie/app/src/main/java/com/example/haba_malek_mesure_glycemie/vue/homeactivity.java
@@ -0,0 +1,30 @@
+package com.example.haba_malek_mesure_glycemie.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.haba_malek_mesure_glycemie.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button btnConsultation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnConsultation = (Button) findViewById(R.id.btnConsultation);
+        btnConsultation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
